4b4c28c89a686633fb4a721c99fdd43e75855408
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2021-03-22 05:47:07 +0100

    Ready to deploy v1.0

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const path = require('path');
 
 const app = express();
 
@@ -23,7 +24,7 @@ app.use(xss());
 //Rate limiting
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10mins
-  max: 100
+  max: 100,
 });
 
 app.use(limiter);
@@ -35,16 +36,22 @@ app.use(hpp());
 
 app.use(express.json({ extended: false }));
 
-app.get(`/`, (req, res) => {
-  res.send('API Running');
-});
-
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/city', require('./routes/api/city'));
 app.use('/api/test', require('./routes/api/test'));
+
+// Serve static assests in production
+
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
